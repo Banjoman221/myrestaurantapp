5dcdf135c803311c5cc75c8a143ea8b3fe047f24
Jacob <jedijacob11@gmail.com>
2018-08-30 16:20:14 -0500

    Made more responsive, add some accessibility, and service worker

--- indexController.js
+++ indexController.js
@@ -3,18 +3,6 @@ RegisterServiceWorker = function () {
     if(!navigator.serviceWorker) return;
 
     navigator.serviceWorker.register('sw.js').then(function (reg) {
-        if ('serviceWorker' in navigator) {
-            // Do a one-off check to see if a service worker's in control.
-            if (navigator.serviceWorker.controller) {
-                console.log('This page is currently controlled by:',
-                navigator.serviceWorker.controller);
-            } else {
-                console.log('This page is not currently controlled ' +
-                'by a service worker.');
-            }
-        } else {
-            console.log('Service workers are not supported.');
-        }
         if(!navigator.serviceWorker.controller) {
             return;
         }
