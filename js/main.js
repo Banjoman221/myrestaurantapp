aa38a55b00049ab0bc89fbd0c81c7698342e2136
Jacob <jedijacob11@gmail.com>
2018-09-05 06:06:43 -0500

    Make more accessible, fix service worker

--- js/main.js
+++ js/main.js
@@ -164,7 +164,7 @@ createRestaurantHTML = (restaurant) => {
     image.alt = restaurant.name + " Restaurant";
     li.append(image);
 
-    const name = document.createElement('h1');
+    const name = document.createElement('h2');
     name.innerHTML = restaurant.name;
     li.append(name);
 
@@ -178,6 +178,7 @@ createRestaurantHTML = (restaurant) => {
 
     const more = document.createElement('a');
     more.innerHTML = 'View Details';
+    more.setAttribute('aria-label', 'View details about ' + restaurant.name + ' restaurant')
     more.href = DBHelper.urlForRestaurant(restaurant);
     li.append(more)
 
--- sw.js
+++ sw.js
@@ -9,14 +9,35 @@ self.addEventListener('install', function (event) {
                 'js/main.js',
                 'js/restaurant_info.js',
                 'css/styles.css',
-                'data/restaurants.json'
+                'data/restaurants.json',
+                '/restaurant.html',
+                '/img/1.jpg',
+                '/img/2.jpg',
+                '/img/3.jpg',
+                '/img/4.jpg',
+                '/img/5.jpg',
+                '/img/6.jpg',
+                '/img/7.jpg',
+                '/img/8.jpg',
+                '/img/9.jpg',
+                '/img/10.jpg',
+                '/img/images/1.jpg',
+                '/img/images/2.jpg',
+                '/img/images/3.jpg',
+                '/img/images/4.jpg',
+                '/img/images/5.jpg',
+                '/img/images/6.jpg',
+                '/img/images/7.jpg',
+                '/img/images/8.jpg',
+                '/img/images/9.jpg',
+                '/img/images/10.jpg',
             ]);
         })
     );
 });
 self.addEventListener('fetch', function (event) {
     event.respondWith(
-        caches.match(event.request).then(function(response) {
+        caches.match(event.request, {ignoreSearch: true}).then(function(response) {
             if (response) {
                 return response;
             }
